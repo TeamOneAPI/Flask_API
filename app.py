b8c940e01c003e1852d7dc0d1ec70588903babf9
Florence106 <caflorence06@gmail.com>
2025-08-28 18:08:01 +0100

    Add swagger UI integration

--- app.py
+++ app.py
@@ -1,3 +1,39 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+from config import Config
+from routes import api_bp
+from auth import auth_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Init extensions
+db.init_app(app)
+jwt = JWTManager(app)
+
+# Register blueprints
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(api_bp, url_prefix="/api")
+
+# @app.route("/")
+# def index():
+#     return "Welcome to BlueWave API! Go to /api/docs for Swagger UI."
+
+
+# Swagger – US-06
+SWAGGER_URL = "/api/docs"
+API_URL = "/static/swagger.json"
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "BlueWave API"}
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
