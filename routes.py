44606698ba1165d1de512de53955f546d48a77e8
Florence106 <caflorence06@gmail.com>
2025-08-31 21:05:04 +0100

    modification to the swagger.json file

--- routes.py
+++ routes.py
@@ -1,2 +1,7 @@
 from flask import request, jsonify, Blueprint
 from flask_jwt_extended import create_access_token, jwt_required, JWTManager
+from datetime import datetime
+from models import db, Observation
+
+api_bp = Blueprint("api", __name__)
+
